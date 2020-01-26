e120fadcdc3453393eca57dd0e3a64e160182ffa
Jonathan Hernández <aggressivepixels@gmail.com>
2020-01-25 20:54:53 -0400

    Move fonts to Style

--- src/Style.purs
+++ src/Style.purs
@@ -2,15 +2,18 @@ module Style (sheet, accentColor) where
 
 import Prelude
 
-import CSS
+import CSS hiding (FontWeight)
 import CSS.Common
+import CSS.FontStyle
 import CSS.ListStyle.Type
 import CSS.Overflow
 import CSS.Size
 import CSS.Text.Whitespace
 import CSS.TextAlign as TA
 import CSS.Text.Transform
-import Data.NonEmpty (singleton)
+import Data.Int as Int
+import Data.Maybe (Maybe(..))
+import Data.NonEmpty ((:|), singleton)
 
 primaryTextColor = rgba 0 0 0 0.87
 secondaryTextColor = rgba 0 0 0 0.6
@@ -19,7 +22,96 @@ separatorColor = rgb 219 219 219
 darkerBackgroundColor = rgb 245 245 245
 
 sheet :: Rendered
-sheet = render do
+sheet = render $ fonts *> styles
+
+fonts :: CSS
+fonts = rubik *> rajdhani *> firaMono
+
+rubik :: CSS
+rubik = do
+  fontFace do
+    fontFaceFamily "Rubik"
+    fontStyle normal
+    fontWeight $ FontWeight 400
+    fontFaceSrc $
+      FontFaceSrcLocal "Rubik Regular" :|
+        [ FontFaceSrcLocal "Rubik-Regular"
+        , FontFaceSrcUrl "../fonts/rubik-v9-latin-regular.woff2" (Just WOFF2)
+        , FontFaceSrcUrl "../fonts/rubik-v9-latin-regular.woff" (Just WOFF)
+        ]
+
+  fontFace do
+    fontFaceFamily "Rubik"
+    fontStyle italic
+    fontWeight $ FontWeight 400
+    fontFaceSrc $
+      FontFaceSrcLocal "Rubik Italic" :|
+        [ FontFaceSrcLocal "Rubik-Italic"
+        , FontFaceSrcUrl "../fonts/rubik-v9-latin-italic.woff2" (Just WOFF2)
+        , FontFaceSrcUrl "../fonts/rubik-v9-latin-italic.woff" (Just WOFF)
+        ]
+
+  fontFace do
+    fontFaceFamily "Rubik"
+    fontStyle normal
+    fontWeight $ FontWeight 500
+    fontFaceSrc $
+      FontFaceSrcLocal "Rubik Medium" :|
+        [ FontFaceSrcLocal "Rubik-Medium"
+        , FontFaceSrcUrl "../fonts/rubik-v9-latin-500.woff2" (Just WOFF2)
+        , FontFaceSrcUrl "../fonts/rubik-v9-latin-500.woff" (Just WOFF)
+        ]
+
+  fontFace do
+    fontFaceFamily "Rubik"
+    fontStyle italic
+    fontWeight $ FontWeight 500
+    fontFaceSrc $
+      FontFaceSrcLocal "Rubik Medium Italic" :|
+        [ FontFaceSrcLocal "Rubik-MediumItalic"
+        , FontFaceSrcUrl "../fonts/rubik-v9-latin-500italic.woff2" (Just WOFF2)
+        , FontFaceSrcUrl "../fonts/rubik-v9-latin-500italic.woff" (Just WOFF)
+        ]
+
+rajdhani :: CSS
+rajdhani = do
+  fontFace do
+    fontFaceFamily "Rajdhani"
+    fontStyle normal
+    -- TODO: This is awkward. Should weight take an Int instead?
+    fontWeight $ FontWeight 400
+    fontFaceSrc $
+      FontFaceSrcLocal "Rajdhani Regular" :|
+        [ FontFaceSrcLocal "Rajdhani-Regular"
+        , FontFaceSrcUrl "../fonts/rajdhani-v9-latin-regular.woff2" (Just WOFF2)
+        , FontFaceSrcUrl "../fonts/rajdhani-v9-latin-regular.woff" (Just WOFF)
+        ]
+
+  fontFace do
+    fontFaceFamily "Rajdhani"
+    fontStyle normal
+    fontWeight $ FontWeight 500
+    fontFaceSrc $
+      FontFaceSrcLocal "Rajdhani-Medium" :|
+        [ FontFaceSrcLocal "Rajdhani-Medium"
+        , FontFaceSrcUrl "../fonts/rajdhani-v9-latin-500.woff2" (Just WOFF2)
+        , FontFaceSrcUrl "../fonts/rajdhani-v9-latin-500.woff" (Just WOFF)
+        ]
+
+firaMono :: CSS
+firaMono = fontFace do
+  fontFaceFamily "Fira Mono"
+  fontStyle normal
+  fontWeight $ FontWeight 400
+  fontFaceSrc $
+    FontFaceSrcLocal "Fira Mono Regular" :|
+      [ FontFaceSrcLocal "FiraMono-Regular"
+      , FontFaceSrcUrl "../fonts/fira-mono-v8-latin-regular.woff2" (Just WOFF2)
+      , FontFaceSrcUrl "../fonts/fira-mono-v8-latin-regular.woff" (Just WOFF)
+      ]
+
+styles :: CSS
+styles = do
   body ? do
     color secondaryTextColor
     fontFamily [ "Rubik" ] (singleton sansSerif)
@@ -78,7 +170,7 @@ sheet = render do
     fontFamily [ "Rajdhani" ] (singleton sansSerif)
     fontSize (em 1.5)
     textTransform uppercase
-    fontWeight bold
+    fontWeight $ FontWeight 500
 
   article ? do
     paddingTop (em 1.5)
@@ -127,3 +219,18 @@ monospace = GenericFontFamily $ fromString "monospace"
 
 time :: Selector
 time = fromString "time"
+
+-- Unfortunately, the CSS library's weight function takes a Number and not an
+-- Int, which then gets written as a floating number instead of an integer.
+-- This (at least in Epiphany) breaks the font weights.
+-- TODO: Make a PR for this.
+
+newtype FontWeight =
+  FontWeight Int
+
+instance valFontWeight :: Val FontWeight where
+  value (FontWeight weight) = value $ show weight
+
+fontWeight :: FontWeight -> CSS
+fontWeight =
+  key $ fromString "font-weight"
