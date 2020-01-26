0ff2e28d820854aaa261cac58ce8b28db10a4691
Jonathan Hernández <aggressivepixels@gmail.com>
2020-01-26 10:30:46 -0400

    Keep accent color in sync between the Styles and Skeleton

--- src/Skeleton.purs
+++ src/Skeleton.purs
@@ -1,14 +1,17 @@
 module Skeleton (view) where
 
 import Prelude
+
+import CSS.Color as Color
 import Data.Maybe (Maybe, maybe)
+import Styles as Styles
 import Text.Smolder.HTML
   ( a, body, footer, head, html, li, link, main, meta, nav, p, title, ul
   )
 import Text.Smolder.HTML.Attributes
   ( charset, className, content, href, lang, name, rel
   )
-import Text.Smolder.Markup (Markup, (!), doctype, text)
+import Text.Smolder.Markup (Markup, doctype, text, (!))
 
 view :: forall e. Maybe String -> Markup e -> Markup e
 view pageTitle pageContent = do
@@ -18,7 +21,7 @@ view pageTitle pageContent = do
       meta ! charset "utf-8"
       meta ! name "viewport" ! content "width=device-width,initial-scale=1"
       -- TODO: This color should be kept in sync with the CSS.
-      meta ! name "theme-color" ! content "rgb(50, 115, 220)"
+      meta ! name "theme-color" ! content (Color.toHexString Styles.accentColor)
       -- TODO: This could also be defined in PureScript code using
       --       purescript-css, which could also help with the previous TODO.
       link ! rel "stylesheet" ! href "/css/styles.css"
