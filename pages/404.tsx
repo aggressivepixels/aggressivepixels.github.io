3ae41d6073420da08d6c64dbd54d6eeb0b25fafb
Jonathan Hernández <aggressivepixels@gmail.com>
2020-07-24 23:31:40 -0400

    Add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import { ReactElement } from 'react'
+import { name as appName } from 'app.json'
+import Layout from 'components/layout'
+import Title from 'components/title'
+import Link from 'next/link'
+
+export default function Contact(): ReactElement {
+  return (
+    <Layout>
+      <Head>
+        <title>Not found &mdash; {appName}</title>
+      </Head>
+      <Title>Not found</Title>
+      <p className="my-6">
+        The page you were looking for doesn&rsquo;t seem to exist.
+      </p>
+      <Link href="/">
+        <a className="text-orange-500">Go home</a>
+      </Link>
+    </Layout>
+  )
+}
