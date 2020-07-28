59e3f3e73a8d125d80dd93d0a5196b1742b652b2
Jonathan Hernández <aggressivepixels@gmail.com>
2020-07-28 18:10:23 -0400

    Responsiveness changes in the nav bar

--- components/nav.tsx
+++ components/nav.tsx
@@ -8,14 +8,14 @@ export default function Nav(): ReactElement {
   const inContact = asPath.startsWith('/contact')
 
   return (
-    <div className="px-4 py-6">
+    <div className="mx-4 py-6 sm:mb-6 sm:flex sm:justify-between">
       <Link href="/">
-        <a className="uppercase font-semibold text-xl pt-1 text-gray-900">
+        <a className="uppercase font-semibold pt-1 select-none text-xl text-gray-900">
           Aggressive
           <span className="text-orange-500">Pixels</span>
         </a>
       </Link>
-      <div className="flex items-baseline">
+      <div className="flex items-baseline sm:items-center">
         <NavLink href="/" title="Home" active={!inBlog && !inContact} />
         <NavLink href="/blog" title="Blog" active={inBlog} />
         <NavLink href="/contact" title="Contact" active={inContact} />
@@ -34,8 +34,8 @@ function NavLink({ href, title, active }: NavLinkProps) {
   return (
     <Link href={href}>
       <a
-        className={`pt-1 mr-3 ${
-          active ? 'text-orange-500 text-xl' : 'text-base'
+        className={`pt-1 mr-3 sm:ml-6 sm:mr-0 ${
+          active ? 'text-orange-500 text-xl sm:text-lg' : 'text-base sm:text-lg'
         }`}
       >
         {title}
