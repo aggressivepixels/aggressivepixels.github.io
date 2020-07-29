f0276a3e6256610a6b80fd5354cdbc591abd3770
Jonathan Hernández <aggressivepixels@gmail.com>
2020-07-28 20:36:23 -0400

    Update navbar style in mobile and remove responsiveness

---
+++ components/icons/menu.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from 'react'
+
+export default function Menu(): ReactElement {
+  return (
+    <svg
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path d="M4 6h16M4 12h16m-7 6h7" />
+    </svg>
+  )
+}
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,26 +1,46 @@
+import Menu from 'components/icons/menu'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 
 export default function Nav(): ReactElement {
   const { asPath } = useRouter()
   const inBlog = asPath.startsWith('/blog')
   const inContact = asPath.startsWith('/contact')
 
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
-    <div className="mx-4 py-6 sm:mb-6 sm:flex sm:justify-between">
-      <Link href="/">
-        <a className="uppercase font-semibold pt-1 select-none text-xl text-gray-900">
-          Aggressive
-          <span className="text-orange-500">Pixels</span>
-        </a>
-      </Link>
-      <div className="flex items-baseline sm:items-center">
+    <nav>
+      <div className="py-4 flex justify-between items-center">
+        <div className="ml-4">
+          <Link href="/">
+            <a className="uppercase font-semibold pt-1 select-none text-xl text-gray-900">
+              Aggressive
+              <span className="text-orange-500">Pixels</span>
+            </a>
+          </Link>
+        </div>
+        <div className="mr-3">
+          <button
+            className={`block h-8 w-8 p-1 rounded ${
+              menuOpen
+                ? 'text-orange-500 bg-orange-500 bg-opacity-25'
+                : 'text-current'
+            }`}
+            onClick={() => setMenuOpen(!menuOpen)}
+            type="button"
+          >
+            <Menu />
+          </button>
+        </div>
+      </div>
+      <div className={`mb-4 mx-3 ${menuOpen ? 'flex flex-col' : 'hidden'}`}>
         <NavLink href="/" title="Home" active={!inBlog && !inContact} />
         <NavLink href="/blog" title="Blog" active={inBlog} />
         <NavLink href="/contact" title="Contact" active={inContact} />
       </div>
-    </div>
+    </nav>
   )
 }
 
@@ -34,9 +54,7 @@ function NavLink({ href, title, active }: NavLinkProps) {
   return (
     <Link href={href}>
       <a
-        className={`pt-1 mr-3 sm:ml-6 sm:mr-0 ${
-          active ? 'text-orange-500 text-xl sm:text-lg' : 'text-base sm:text-lg'
-        }`}
+        className={`text-xl pt-1 px-1 mb-1 ${active ? 'text-orange-500' : ''}`}
       >
         {title}
       </a>
