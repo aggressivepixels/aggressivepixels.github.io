3c847980762b59bbe42f953a5a5269658c937126
Jonathan Hernández <aggressivepixels@gmail.com>
2020-08-01 13:42:58 -0400

    Fix layout spacing

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: Props): ReactElement {
   return (
     <div className="max-w-screen-md mx-auto">
       <Nav />
-      <main className="px-4 mt-6 sm:mt-0 sm:my-6">{children}</main>
+      <main className="px-4 mt-6 sm:mb-6">{children}</main>
     </div>
   )
 }
